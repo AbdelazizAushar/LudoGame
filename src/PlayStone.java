bb5151e583249689c822a7b9879493dca4214906
TimaDawaa <tima302t@gmail.com>
2024-12-30 10:23:57 +0300

    constructor

--- src/PlayStone.java
+++ src/PlayStone.java
@@ -1,6 +1,17 @@
 public class PlayStone {
+
+
         char color ;
         int i;
+
         boolean isOut;
         boolean isAWin;
+
+
+        public PlayStone(char color, int i, boolean isOut, boolean isAWin) {
+                this.color = color;
+                this.i = i;
+                this.isOut = isOut;
+                this.isAWin = isAWin;
+        }
 }
