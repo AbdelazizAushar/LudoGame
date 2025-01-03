094bac29e081b4c343db4722d96a771ac79d21e8
Abdelaziz <abdeez.aushar@yandex.com>
2025-01-03 18:52:08 +0300

    Get Movable Stones

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Player {
     boolean isWin;
@@ -39,5 +40,30 @@ public class Player {
         }
     }
 
+    public ArrayList<PlayStone> getMovableStones(State state, int dice) {
+        ArrayList<PlayStone> movableStones = new ArrayList<>();
+        for (PlayStone stone : stones) {
+            if (stone.isAWin) continue;
+            if (stone.isOut && dice != 6) continue;
+            if (state.BlockFounded(dice, stone)) continue;
+            if (getWinningTileIndex() != stone.i + dice) continue;
+            movableStones.add(stone);
+        }
+        return movableStones;
+    }
 
+    private int getWinningTileIndex() {
+        ArrayList<PlayStone> winningStones = getStonesWinningInOrder();
+        PlayStone lastStoneToWin = winningStones.getFirst(); // the most outside stone
+        return lastStoneToWin.i - 1; // winning tile index
+    }
+
+    private ArrayList<PlayStone> getStonesWinningInOrder(){
+        ArrayList<PlayStone> winningStones = new ArrayList<>();
+        for (PlayStone stone : stones) {
+            if (stone.isAWin) winningStones.add(stone);
+        }
+        winningStones.sort(Comparator.comparingInt(stone -> stone.i));
+        return winningStones;
+    }
 }
